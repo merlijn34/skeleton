854a01296c55164df5597022ce97bf6f8111987e
Merlijn van den Berg <merlijn34@gmail.com>
2016-06-22 15:35:45 +0200

    init commit

---
+++ public/pages/elements/index.php
@@ -0,0 +1,259 @@
+<!-- 
+ 
+  __  __ _____ ____  _     ___    _ _   _  __     ___    _   _   ____  _____ _   _   ____  _____ ____   ____ 
+ |  \/  | ____|  _ \| |   |_ _|  | | \ | | \ \   / / \  | \ | | |  _ \| ____| \ | | | __ )| ____|  _ \ / ___|
+ | |\/| |  _| | |_) | |    | |_  | |  \| |  \ \ / / _ \ |  \| | | | | |  _| |  \| | |  _ \|  _| | |_) | |  _ 
+ | |  | | |___|  _ <| |___ | | |_| | |\  |   \ V / ___ \| |\  | | |_| | |___| |\  | | |_) | |___|  _ <| |_| |
+ |_|  |_|_____|_| \_\_____|___\___/|_| \_|    \_/_/   \_\_| \_| |____/|_____|_| \_| |____/|_____|_| \_\\____|
+
+-->
+
+
+
+
+
+
+
+<?php include "../../sections/head.php" ?>
+
+<section class="topbar">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-4">
+                Logo
+            </div>
+        </div>
+    </div>
+</section>
+<header class="jumbotron">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>H1 Jumbotron</h1>
+                <p class="lead">Dit is een p lead, <strong>consectetuer</strong> adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
+                penatibus et magnis dis <strong>parturient</strong> montes, nascetur ridiculus mus.
+                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            </p>
+                <a href="" class="button">Click here</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<section class="content-primary">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4">
+                <h2>H2</h2>
+                <p>Lorem ipsum dolor sit amet, <strong>consectetuer</strong> adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
+                penatibus et magnis dis <strong>parturient</strong> montes, nascetur ridiculus mus.
+                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                Nulla consequat massa quis enim.</p>
+                <a href="" class="button">Click here</a>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <h2>H2</h2>
+                <ol>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                </ol>
+                <ul>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                    <li>Dit is een list item</li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <h2>H2</h2>
+                <form>
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Name">
+                    <label for="selectfield">Selectfield</label>
+                    <div class="styled-select">
+                        <select class="form-control" id="selectfield">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                    </div>
+                    <label for="textarea">Textarea</label>
+                    <textarea class="form-control" id="textarea" rows="3"></textarea>
+                    <input type="checkbox" id="cbox" value="cbox"> <label for="cbox">Click to check</label>
+                    <input type="checkbox" id="cbox2" value="cbox2"> <label for="cbox2">Click to check</label>
+                    <div class="clearfix">
+                    </div>
+                    <button type="submit" class="button">Submit</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-secondary">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4">
+                <article class="card">
+                    <header class="card-header">
+                        <h3>h3</h3>
+                    </header>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="" class="button">Click here</a>
+                    </footer>
+                </article>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <article class="card">
+                    <header class="card-header">
+                        <h3>h3</h3>
+                    </header>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="" class="button">Click here</a>
+                    </footer>
+                </article>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <article class="card">
+                    <header class="card-header">
+                        <h3>h3</h3>
+                    </header>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="" class="button">Click here</a>
+                    </footer>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-primary">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <blockquote>
+                    <p>“One Ring, to Rule them All.”</p>
+                    <footer>Tolkien in <cite title="The Lord Of The Rings">The Lord Of The Rings</cite>
+                </blockquote>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="media">
+                    <div class="media-left">
+                          <img class="media-object" src="http://placehold.it/100x100" alt="...">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">Teammember</h4>
+                        <div class="meta">Meta data</div>
+                        <p>Lorem ipsum dolor sit amet, <strong>consectetuer</strong> adipiscing elit.
+                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
+                        penatibus et magnis dis <strong>parturient</strong> montes, nascetur ridiculus mus.
+                        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                        </p>
+                    </div>
+                </div>
+                <div class="media">
+                    <div class="media-left">
+                          <img class="media-object" src="http://placehold.it/50x50" alt="...">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">Teammember</h4>
+                        <div class="meta">Meta data</div>
+                        <p>Lorem ipsum dolor sit amet, <strong>consectetuer</strong> adipiscing elit.
+                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
+                        penatibus et magnis dis <strong>parturient</strong> montes, nascetur ridiculus mus.
+                        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content content-secondary">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                <h3>Lists</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-link">
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-check">
+                    <li>Here's a list item</li>
+                    <li>Here's a list item</li>
+                    <li>Here's a list item</li>
+                    <li>Here's a very long list item to fill multiple lines of text</li>
+                    <li>Here's a list item</li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-arrow">
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a link</a></li>
+                    <li><a href="">Here's a very long link to fill multiple lines of text</a></li>
+                    <li><a href="">Here's a link</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <a href="" class="media">
+     
+                    <div class="media-body">
+                        <div class="meta"><small>Meta data</small></div>
+                        <h5 class="media-heading">Item title</h5>
+                    </div>
+                </a>
+                 <a href="" class="media">
+                    <div class="media-left media-middle">
+                          <img class="media-object" src="http://placehold.it/50x50" alt="...">
+                    </div>
+                    <div class="media-body">
+                        <h5 class="media-heading">Item title</h5>
+                        <div class="meta"><small>Meta data</small></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="content-footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <h4>Blijf op de hoogte</h4>
+                <ul class="list-social">
+                    <li><a href="" class="icon-social icon-facebook" target="_blank"></a></li>
+                    <li><a href="" class="icon-social icon-twitter" target="_blank"></a></li>
+                    <li><a href="" class="icon-social icon-instagram" target="_blank"></a></li>
+                    <li><a href="" class="icon-social icon-linkedin" target="_blank"></a></li>
+                    <li><a href="" class="icon-social icon-whatsapp" target="_blank"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+<?php include "../../sections/footer.php" ?>
---
+++ public/sections/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- ADD PAGE TITLE & DELETE THIS COMMENT-->
+    <title>iSKELETON</title>
+
+    <!-- REPLACE THE PLACEHOLDER PNG & DELETE THIS COMMENT-->
+    <link rel="icon" href="../../images/favicon.png" type="image/png"/>
+
+    <!--  ADD SHARE DATA & DELETE THIS COMMENT -->
+    <meta property="og:type" content="blog" />
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+
+    <link href="../../css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
